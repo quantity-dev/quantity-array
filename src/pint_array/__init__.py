44802751bf1c955ece0c48fdae932093a64632c2
Lucas Colley <lucas.colley8@gmail.com>
2024-12-30 13:59:23 +0000

    add some methods

--- src/pint_array/__init__.py
+++ src/pint_array/__init__.py
@@ -32,7 +32,7 @@ def pint_namespace(xp):
             self._shape = magnitude.shape
             self._size = magnitude.size
 
-        # __array_priority__ = 1  # make reflected operators work with NumPy
+        __array_priority__ = 1  # make reflected operators work with NumPy
 
         @property
         def dtype(self):
@@ -61,8 +61,8 @@ def pint_namespace(xp):
                 raise NotImplementedError()
             
         def _call_super_method(self, method_name, *args, **kwargs):
-            method = getattr(self.data, method_name)
-            args = [getattr(arg, 'data', arg) for arg in args]
+            method = getattr(self.magnitude, method_name)
+            args = [getattr(arg, 'magnitude', arg) for arg in args]
             return method(*args, **kwargs)
 
         ## Indexing ##
@@ -130,239 +130,56 @@ def pint_namespace(xp):
             _magnitude = self._magnitude.to_device(device, stream=stream)
             return ArrayUnitQuantity(_magnitude, self.units)
 
-        # def _numpy_method_wrap(self, func, *args, **kwargs):
-        #     """Convenience method to wrap on the fly NumPy ndarray methods taking
-        #     care of the units.
-        #     """
-
-        #     # Set input units if needed
-        #     if func.__name__ in set_units_ufuncs:
-        #         self.__ito_if_needed(set_units_ufuncs[func.__name__][0])
-
-        #     value = func(*args, **kwargs)
-
-        #     # Set output units as needed
-        #     if func.__name__ in (
-        #         matching_input_copy_units_output_ufuncs
-        #         + copy_units_output_ufuncs
-        #         + self._wrapped_numpy_methods
-        #     ):
-        #         output_unit = self._units
-        #     elif func.__name__ in set_units_ufuncs:
-        #         output_unit = set_units_ufuncs[func.__name__][1]
-        #     elif func.__name__ in matching_input_set_units_output_ufuncs:
-        #         output_unit = matching_input_set_units_output_ufuncs[func.__name__]
-        #     elif func.__name__ in op_units_output_ufuncs:
-        #         output_unit = get_op_output_unit(
-        #             op_units_output_ufuncs[func.__name__],
-        #             self.units,
-        #             list(args) + list(kwargs.values()),
-        #             self._magnitude.size,
-        #         )
-        #     else:
-        #         output_unit = None
-
-        #     if output_unit is not None:
-        #         return self.__class__(value, output_unit)
-
-        #     return value
-
-        # def __array__(self, t=None) -> np.ndarray:
-        #     if HAS_NUMPY and isinstance(self._magnitude, np.ndarray):
-        #         warnings.warn(
-        #             "The unit of the quantity is stripped when downcasting to ndarray.",
-        #             UnitStrippedWarning,
-        #             stacklevel=2,
-        #         )
-        #     return _to_magnitude(self._magnitude, force_ndarray=True)
-
-        # def clip(self, min=None, max=None, out=None, **kwargs):
-        #     if min is not None:
-        #         if isinstance(min, self.__class__):
-        #             min = min.to(self).magnitude
-        #         elif self.dimensionless:
-        #             pass
-        #         else:
-        #             raise DimensionalityError("dimensionless", self._units)
-
-        #     if max is not None:
-        #         if isinstance(max, self.__class__):
-        #             max = max.to(self).magnitude
-        #         elif self.dimensionless:
-        #             pass
-        #         else:
-        #             raise DimensionalityError("dimensionless", self._units)
-        #     return self.__class__(self.magnitude.clip(min, max, out, **kwargs), self._units)
-
-        # def fill(self: NumpyQuantity, value) -> None:
-        #     self._units = value._units
-        #     return self.magnitude.fill(value.magnitude)
-
-        # def put(self: NumpyQuantity, indices, values, mode="raise") -> None:
-        #     if isinstance(values, self.__class__):
-        #         values = values.to(self).magnitude
-        #     elif self.dimensionless:
-        #         values = self.__class__(values, "").to(self)
-        #     else:
-        #         raise DimensionalityError("dimensionless", self._units)
-        #     self.magnitude.put(indices, values, mode)
-
-        # @property
-        # def real(self) -> NumpyQuantity:
-        #     return self.__class__(self._magnitude.real, self._units)
-
-        # @property
-        # def imag(self) -> NumpyQuantity:
-        #     return self.__class__(self._magnitude.imag, self._units)
-
-        # @property
-        # def T(self):
-        #     return self.__class__(self._magnitude.T, self._units)
-
-        # @property
-        # def flat(self):
-        #     for v in self._magnitude.flat:
-        #         yield self.__class__(v, self._units)
-
-        # @property
-        # def shape(self) -> Shape:
-        #     return self._magnitude.shape
-
-        # @property
-        # def dtype(self):
-        #     return self._magnitude.dtype
-
-        # @shape.setter
-        # def shape(self, value):
-        #     self._magnitude.shape = value
-
-        # def searchsorted(self, v, side="left", sorter=None):
-        #     if isinstance(v, self.__class__):
-        #         v = v.to(self).magnitude
-        #     elif self.dimensionless:
-        #         v = self.__class__(v, "").to(self)
-        #     else:
-        #         raise DimensionalityError("dimensionless", self._units)
-        #     return self.magnitude.searchsorted(v, side)
-
-        # def dot(self, b):
-        #     """Dot product of two arrays.
-
-        #     Wraps np.dot().
-        #     """
-
-        #     return np.dot(self, b)
-
-        # @method_wraps("prod")
-        # def prod(self, *args, **kwargs):
-        #     """Return the product of quantity elements over a given axis
-
-        #     Wraps np.prod().
-        #     """
-        #     return np.prod(self, *args, **kwargs)
-
-        # def __ito_if_needed(self, to_units):
-        #     if self.unitless and to_units == "radian":
-        #         return
-
-        #     self.ito(to_units)
-
-        # def __len__(self) -> int:
-        #     return len(self._magnitude)
-
-        # def __getattr__(self, item) -> Any:
-        #     if item.startswith("__array_"):
-        #         # Handle array protocol attributes other than `__array__`
-        #         raise AttributeError(f"Array protocol attribute {item} not available.")
-        #     elif item in HANDLED_UFUNCS or item in self._wrapped_numpy_methods:
-        #         magnitude_as_duck_array = _to_magnitude(
-        #             self._magnitude, force_ndarray_like=True
-        #         )
-        #         try:
-        #             attr = getattr(magnitude_as_duck_array, item)
-        #             return functools.partial(self._numpy_method_wrap, attr)
-        #         except AttributeError:
-        #             raise AttributeError(
-        #                 f"NumPy method {item} not available on {type(magnitude_as_duck_array)}"
-        #             )
-        #         except TypeError as exc:
-        #             if "not callable" in str(exc):
-        #                 raise AttributeError(
-        #                     f"NumPy method {item} not callable on {type(magnitude_as_duck_array)}"
-        #                 )
-        #             else:
-        #                 raise exc
-        #     elif (
-        #         HAS_UNCERTAINTIES and item == "ndim" and isinstance(self._magnitude, UFloat)
-        #     ):
-        #         # Dimensionality of a single UFloat is 0, like any other scalar
-        #         return 0
-
-        #     try:
-        #         return getattr(self._magnitude, item)
-        #     except AttributeError:
-        #         raise AttributeError(
-        #             "Neither Quantity object nor its magnitude ({}) "
-        #             "has attribute '{}'".format(self._magnitude, item)
-        #         )
-
-        # def __getitem__(self, key):
-        #     try:
-        #         return type(self)(self._magnitude[key], self._units)
-        #     except PintTypeError:
-        #         raise
-        #     except TypeError:
-        #         raise TypeError(
-        #             "Neither Quantity object nor its magnitude ({})"
-        #             "supports indexing".format(self._magnitude)
-        #         )
-
-        # def __setitem__(self, key, value):
-        #     try:
-        #         # If we're dealing with a masked single value or a nan, set it
-        #         if (
-        #             isinstance(self._magnitude, np.ma.MaskedArray)
-        #             and np.ma.is_masked(value)
-        #             and getattr(value, "size", 0) == 1
-        #         ) or (getattr(value, "ndim", 0) == 0 and math.isnan(value)):
-        #             self._magnitude[key] = value
-        #             return
-        #     except TypeError:
-        #         pass
-
-        #     try:
-        #         if isinstance(value, self.__class__):
-        #             factor = self.__class__(
-        #                 value.magnitude, value._units / self._units
-        #             ).to_root_units()
-        #         else:
-        #             factor = self.__class__(value, self._units ** (-1)).to_root_units()
-
-        #         if isinstance(factor, self.__class__):
-        #             if not factor.dimensionless:
-        #                 raise DimensionalityError(
-        #                     value,
-        #                     self.units,
-        #                     extra_msg=". Assign a quantity with the same dimensionality "
-        #                     "or access the magnitude directly as "
-        #                     f"`obj.magnitude[{key}] = {value}`.",
-        #                 )
-        #             self._magnitude[key] = factor.magnitude
-        #         else:
-        #             self._magnitude[key] = factor
-
-        #     except PintTypeError:
-        #         raise
-        #     except TypeError as exc:
-        #         raise TypeError(
-        #             f"Neither Quantity object nor its magnitude ({self._magnitude}) "
-        #             "supports indexing"
-        #         ) from exc
-
-
     class ArrayUnitQuantity(ArrayQuantity, Quantity):
         pass
 
+
+    ## Methods ##
+
+    # Methods that return the result of a unary operation as an array
+    unary_names = (
+        ['__abs__', '__floordiv__', '__invert__', '__neg__', '__pos__', '__ceil__']
+    )
+    for name in unary_names:
+        def fun(self, name=name):
+            return ArrayUnitQuantity(self._call_super_method(name), self.units)
+        setattr(ArrayQuantity, name, fun)
+
+    # Methods that return the result of a unary operation as a Python scalar
+    unary_names_py = ['__bool__', '__complex__', '__float__', '__index__', '__int__']
+    for name in unary_names_py:
+        def fun(self, name=name):
+            return self._call_super_method(name)
+        setattr(ArrayQuantity, name, fun)
+
+    # # Methods that return the result of an elementwise binary operation
+    # binary_names = ['__add__', '__sub__', '__and__', '__eq__', '__ge__', '__gt__',
+    #                 '__le__', '__lshift__', '__lt__', '__mod__', '__mul__', '__ne__',
+    #                 '__or__', '__pow__', '__rshift__', '__sub__', '__truediv__',
+    #                 '__xor__'] + ['__divmod__', '__floordiv__']
+    # # Methods that return the result of an elementwise binary operation (reflected)
+    # rbinary_names = ['__radd__', '__rand__', '__rdivmod__', '__rfloordiv__',
+    #                 '__rlshift__', '__rmod__', '__rmul__', '__ror__', '__rpow__',
+    #                 '__rrshift__', '__rsub__', '__rtruediv__', '__rxor__']
+    # for name in binary_names + rbinary_names:
+    #     def fun(self, other, name=name):
+    #         mask = (self.mask | other.mask) if hasattr(other, 'mask') else self.mask
+    #         data = self._call_super_method(name, other)
+    #         return ArrayUnitQuantity(data, mask)
+    #     setattr(ArrayQuantity, name, fun)
+
+    # # In-place methods
+    # desired_names = ['__iadd__', '__iand__', '__ifloordiv__', '__ilshift__',
+    #                 '__imod__', '__imul__', '__ior__', '__ipow__', '__irshift__',
+    #                 '__isub__', '__itruediv__', '__ixor__']
+    # for name in desired_names:
+    #     def fun(self, other, name=name, **kwargs):
+    #         if hasattr(other, 'mask'):
+    #             # self.mask |= other.mask doesn't work because mask has no setter
+    #             self.mask.__ior__(other.mask)
+    #         self._call_super_method(name, other)
+    #         return self
+    #     setattr(ArrayQuantity, name, fun)
 
     def asarray(obj, /, *, units=None, dtype=None, device=None, copy=None):
         if device is not None:
